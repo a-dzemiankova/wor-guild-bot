3ee2b4079df19b39801ccd3a9033737e437fd488
Anastasia Demenkova <a.piatrovskaya@gmail.com>
2024-11-15 18:34:56 +0300

    Modified interface to delete unnecessary messages

--- bot_interface.py
+++ bot_interface.py
@@ -19,19 +19,28 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: call.data in ['search', 'restart'])
 def choose_character(call):
-    if call.data == 'restart':
-        config.clear()
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    btn1 = types.InlineKeyboardButton('Ингрид', callback_data='id_Ингрид')
-    btn2 = types.InlineKeyboardButton('Аякс', callback_data='id_Аякс')
-    btn3 = types.InlineKeyboardButton('Саргак', callback_data='id_Саргак')
-    btn4 = types.InlineKeyboardButton('Претус', callback_data='id_Претус')
-    btn5 = types.InlineKeyboardButton('Кир', callback_data='id_Кир')
-    btn6 = types.InlineKeyboardButton('Хамет', callback_data='id_Хамет')
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-    current_list = '\n'.join(f"{k} - {v}" for k, v in config.items()) if config else '\nПока пусто\n'
-    bot.send_message(call.message.chat.id, f'Уже в списке:\n\n {current_list}\n\nВыберите героя для добавления в список:',
-                     reply_markup=markup)
+    if len(config) == 2:
+        res = '\n'.join(f"{k} - {v}" for k, v in config.items())
+        markup = types.InlineKeyboardMarkup(row_width=1)
+        btn1 = types.InlineKeyboardButton('Подтвердить', callback_data='confirm')
+        btn2 = types.InlineKeyboardButton('Начать заново', callback_data='restart')
+        markup.add(btn1, btn2)
+        bot.send_message(call.message.chat.id, f" Итоговый список:\n\n{res}", reply_markup=markup)
+    else:
+        if call.data == 'restart':
+            config.clear()
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        btn1 = types.InlineKeyboardButton('Ингрид', callback_data='id_Ингрид')
+        btn2 = types.InlineKeyboardButton('Аякс', callback_data='id_Аякс')
+        btn3 = types.InlineKeyboardButton('Саргак', callback_data='id_Саргак')
+        btn4 = types.InlineKeyboardButton('Претус', callback_data='id_Претус')
+        btn5 = types.InlineKeyboardButton('Кир', callback_data='id_Кир')
+        btn6 = types.InlineKeyboardButton('Хамет', callback_data='id_Хамет')
+        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+        current_list = '\n'.join(f"{k} - {v}" for k, v in config.items()) if config else '\n**Пока пусто**\n'
+        bot.delete_message(call.message.chat.id, call.message.id)
+        bot.send_message(call.message.chat.id, f'Уже в списке:\n\n {current_list}\n\nВыберите героя для добавления в список:',
+                         reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('id'))
@@ -43,7 +52,8 @@ def choose_evo(call):
     btn2 = types.InlineKeyboardButton('2', callback_data=f'evo_{character}_2')
     btn3 = types.InlineKeyboardButton('3', callback_data=f'evo_{character}_3')
     markup.add(btn1, btn2, btn3)
-    bot.send_message(call.message.chat.id, 'Выберите пробуду:', reply_markup=markup)
+    bot.edit_message_text('Выберите пробуду:', call.message.chat.id, call.message.id)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.id, reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('evo'))
@@ -53,24 +63,27 @@ def manage_config(call):
     evo = data[2]
     config[character] = evo
     print(config)
-    markup = types.InlineKeyboardMarkup(row_width=1)
+    markup = types.InlineKeyboardMarkup(row_width=2)
     btn1 = types.InlineKeyboardButton('Продолжить', callback_data='search')
-    markup.add(btn1)
-    bot.send_message(call.message.chat.id, f'Ваш выбор:\n {character} - {evo}', reply_markup=markup)
-    if len(config) == 5:
-        res = '\n'.join(f"{k} - {v}" for k, v in config.items())
-        markup = types.InlineKeyboardMarkup(row_width=1)
-        btn1 = types.InlineKeyboardButton('Подтвердить', callback_data='confirm')
-        btn2 = types.InlineKeyboardButton('Начать заново', callback_data='restart')
-        markup.add(btn1, btn2)
-        bot.send_message(call.message.chat.id, f" Итоговый список:\n\n {res}", reply_markup=markup)
+    btn2 = types.InlineKeyboardButton('Изменить выбор', callback_data=f'change_{character}')
+    markup.add(btn1, btn2)
+    bot.delete_message(call.message.chat.id, call.message.id)
+    bot.send_message(call.message.chat.id, f'Ваш выбор:\n{character} - {evo}', reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: call.data.startswith('change'))
+def change_choice(call):
+    data = call.data.split('_')
+    character = data[1]
+    config.pop(character)
+    choose_character(call)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'confirm')
 def find_players(call):
-    bot.send_message(call.message.chat.id, 'Тут будут подходящие игроки: ')
+    bot.delete_message(call.message.chat.id, call.message.id - 1)
+    bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.id, text='Text')
     # логика с фильтром подсходящих игроков + кнопка "Начать заново" через колбэк restart
-
 
 
 bot.infinity_polling()
